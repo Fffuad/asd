de74cf7b7d59976aa81afd6b25b9a5014d5021e3
MSI <fuadbayramov.fb@gmail.com>
2023-04-30 09:11:14 -0800

    new 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-}public void foo(){
+    }
 
+    public void foo() {
     }
-    }
+
+
+    private String name;
+    private int age;
+}
