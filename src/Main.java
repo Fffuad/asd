6c7e697edbdad4bc1415628b57683533ccc22c47
MSI <fuadbayramov.fb@gmail.com>
2023-04-30 09:27:35 -0800

    new 4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("3cu elave---");
         System.out.println("4cu elave----");
+        System.out.println("4cu elave----");
 
     }
 
